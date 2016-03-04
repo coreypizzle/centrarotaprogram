3bb8cb952967080da9e4ff31037a451b4298d750
Corey Purdy <Corey Purdy>
2016-03-04 20:27:29 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229193421) do
+ActiveRecord::Schema.define(version: 20160302191725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 20160229193421) do
     t.string   "fridayap04"
     t.string   "saturdayap04"
     t.string   "sundayap04"
+    t.integer  "order",                  default: 0
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
